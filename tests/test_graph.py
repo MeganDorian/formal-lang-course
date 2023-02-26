314685d6455b80171db9957c098ad9216435a0f6
megan <mishakollins68@gmail.com>
2023-02-26 11:10:30 +0300

    fix: added named tuple and changed method name for two cycled graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,19 +3,19 @@ from project.task01.graph import *
 
 def test_graph_info():
     graph_name = "ls"
-    V, E, L = graph_info(graph_name)
-    assert 1687 == V
-    assert 1453 == E
-    assert ["d", "a"] == L
+    graph = graph_info(graph_name)
+    assert 1687 == graph.nodes
+    assert 1453 == graph.edges
+    assert ["d", "a"] == graph.labels
 
 
-def test_create_graph():
-    filename = "test_create_graph.dot"
+def test_create_and_save_to_dot_labeled_two_cycles_graph_graph():
+    filename = "test_create_labeled_two_cycles_graph.dot"
     expected = (
         "digraph  { 1; 2; 0; 3; 4; 5; 6; 1 -> 2  [key=0, label=a]; 2 -> 0  [key=0, label=a]; 0 -> 1  [key=0, "
         "label=a]; 0 -> 3  [key=0, label=c]; 3 -> 4  [key=0, label=c]; 4 -> 5  [key=0, label=c]; 5 -> 6  ["
         "key=0, label=c]; 6 -> 0  [key=0, label=c]; } "
     )
-    create_graph(2, 4, ["a", "c"], filename)
+    create_and_save_to_dot_labeled_two_cycles_graph_graph(2, 4, ["a", "c"], filename)
     with open(filename, "r") as f:
         assert expected == f.read().replace("\n", " ")
